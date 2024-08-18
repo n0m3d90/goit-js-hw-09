c54f01a10f2d617ba23153333f3837ddcb1d8ef9
n0m3d90 <kivu_flrn90@yahoo.com>
2024-08-18 21:40:00 +0300

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3; 
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  const delayInput = parseInt(event.target.delay.value); 
+  const stepInput = parseInt(event.target.step.value); 
+  const amountInput = parseInt(event.target.amount.value); 
+
+  let currentDelay = delayInput; 
+
+  for (let i = 1; i <= amountInput; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += stepInput; 
+  }
+});
