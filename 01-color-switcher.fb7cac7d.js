e844ffc0b26493a36212e1e5541494c8d015b84b
n0m3d90 <162443513+n0m3d90@users.noreply.github.com>
2024-08-18 18:40:21 +0000

    Deploying to gh-pages from @ n0m3d90/goit-js-hw-09@c54f01a10f2d617ba23153333f3837ddcb1d8ef9 🚀

---
+++ 01-color-switcher.fb7cac7d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.fb7cac7d.js.map
